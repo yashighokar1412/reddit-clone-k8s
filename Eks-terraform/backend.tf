9ba2576ecc766845a1ba9a4d6e204aa51119e8fb
yashighokar1412 <165381744+yashighokar1412@users.noreply.github.com>
2024-06-07 19:01:11 +0530

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -2,6 +2,6 @@ terraform {
   backend "s3" {
     bucket = "yash1412" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
-    region = "us-east-1"
+    region = "ap-south-1"
   }
 }
